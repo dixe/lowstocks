ebf2bf2575d389fe18bf9a16122ad31fba794601
Nikolaj Østergaard <niko.fris@gmail.com>
2025-07-27 15:38:50 +0200

    Removed unused libs Skip tickers with . and ending in W Load only a bit of the data

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use csv::Reader;
 use serde;
 use serde_with::{serde_as, Bytes};
 use std::collections::HashMap;
-use rkyv::{deserialize, rancor::Error, Archive, Deserialize, Serialize};
 use std::fs;
 use std::path::PathBuf;
 
@@ -22,7 +21,7 @@ struct RecordCsv {
 
 
 
-#[derive(Debug, Clone, Default, Archive, Deserialize, Serialize, PartialEq)]
+#[derive(Debug, Copy, Clone, Default)]
 // record without ticker, store ticker as hashmap key
 struct Record {
     volume: i64,
@@ -44,6 +43,10 @@ fn load_data(path: &PathBuf, records: &mut HashMap<String, Vec::<Record>>) {
     for result in rdr.deserialize() {
         let record_csv: RecordCsv = result.unwrap();
 
+        if record_csv.ticker.contains('.') || record_csv.ticker.ends_with('W') {
+            continue;
+        }
+
         let record = Record {
             volume : record_csv.volume,
             open : record_csv.open,
@@ -55,7 +58,7 @@ fn load_data(path: &PathBuf, records: &mut HashMap<String, Vec::<Record>>) {
         };
 
         if !records.contains_key(&record_csv.ticker) {
-            records.insert(&record_csv.ticker, vec![]);
+            records.insert(record_csv.ticker.clone(), vec![]);
         }
 
         if let Some(val) = records.get_mut(&record_csv.ticker) { val.push(record); };
@@ -63,35 +66,28 @@ fn load_data(path: &PathBuf, records: &mut HashMap<String, Vec::<Record>>) {
 }
 
 
-fn get_or_insert_ticker_id(connection: &Connection, ticker_name: &str) -> u64 {
-    1
-}
-
-fn insert_record(connection: &Connection, ticker_id: u64, record: Record) {
-
-}
-
 fn main() {
 
     let paths = fs::read_dir("G:\\OttoData\\").unwrap();
 
-    let connection = sqlite::open("G:\\OttoData\\records.db").unwrap();
+    let mut records = HashMap::new();
 
-    for path in paths {
+    let mut days = 0;
+    for path in  paths {
+        if days > 14 {
+            break;
+        }
 
-        let mut records = HashMap::new();
-
+        println!("{:?}", &path);
         load_data(&path.unwrap().path(), &mut records);
 
-        for k in records.keys() {
-            let ticker_id = get_or_insert_ticker_id(k);
 
-            for r in records[k] {
-                insert_record(ticker_id, &r);
-            }
-        }
-        panic!();
+        days += 1;
     }
+
+    println!("Loaded {:?} data", days);
+
+    println!("Keys {:?}", records.keys().len());
 }
 
 
@@ -99,6 +95,8 @@ fn main() {
 fn vec_str_to_u64(v : &Vec::<u8>) -> u64 {
 
     let mut ticker : u64 = 0;
+
+
     for b in v {
         ticker = ticker << 8;
         ticker = ticker + *b as u64;
@@ -114,54 +112,4 @@ fn copy_ticker_to_buffer(ticker : u64, buffer: &mut [char;5]) {
     buffer[2] = ((ticker >> 16 & 0xFF) as u8) as char;
     buffer[1] = ((ticker >> 24 & 0xFF) as u8) as char;
     buffer[0] = ((ticker >> 32 & 0xFF) as u8) as char;
-}
-
-
-
-
-
-// load into vec, then into hashmap when ticker changes
-fn load_data2(path: &PathBuf) -> HashMap<String, Vec::<Record>> {
-
-    // Build the CSV reader and iterate over each record.
-    let mut rdr = Reader::from_path(path).unwrap();
-    let mut records = HashMap::new();
-    let mut cur_vec = vec![];
-    let mut cur_ticker = 0;
-    let mut i = 0;
-    for result in rdr.deserialize() {
-        let record_csv: RecordCsv = result.unwrap();
-
-
-        let record = Record {
-            volume : record_csv.volume,
-            open : record_csv.open,
-            close : record_csv.close,
-            high : record_csv.high,
-            low : record_csv.low,
-            window_start : record_csv.window_start,
-            transactions : record_csv.transactions
-        };
-
-
-        if cur_ticker == 0 {
-            cur_ticker = ticker;
-        }
-
-        // add cur data to hashmap and reset tmp vec
-        if cur_ticker != ticker {
-            records.insert(cur_ticker, cur_vec);
-            cur_ticker = ticker;
-            cur_vec = vec![];
-        }
-
-        cur_vec.push(record);
-    }
-
-
-    // add last cur ticker
-    records.insert(cur_ticker, cur_vec);
-
-
-    return records;
 }
